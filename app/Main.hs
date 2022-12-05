79c6134ca3f8c6271594b7fe845f2eeb04aac838
Itoh Haruka <liaoy551@gmail.com>
2022-12-05 12:11:38 +0900

    1205

--- app/Main.hs
+++ app/Main.hs
@@ -7,37 +7,16 @@ readInt :: String -> Int
 readInt = read
 
 main :: IO ()
-main = interact func
+-- main = interact func
+main = interact (drive func2)
+
+drive :: ([String] -> [String]) -> (String -> String)
+drive f = unlines . f . lines
 
 func :: String -> String
+func input = case map (map readInt . words) (lines input) of
+    _ -> unlines []
 
--- func :: String -> String
--- func input = unlines ["Hello Algo-method!"]
-
--- func :: String -> String
--- func input = unlines (map showInt [1 .. 3])
-
--- func :: String -> String
--- func input = showInt (27182 `mod` 818)
-
--- func :: String -> String
--- func input = showInt (314 * (159 + 265) - 358)
-
--- func :: String -> String
--- func input = showInt (60 * 60 * 24)
-
--- func input = case readInt input of
---     n -> showInt (2 * n)
-
--- func input = case readInt input of
---     n -> showInt (n `mod` 5)
-
--- func input = case words input of
---     s:_ -> unlines [concat (replicate 3 s)]
---     []  -> error "invalid input"
-
--- func input = case map words (lines input) of
---     [[s]] -> unlines[[s !! 2]]
-
--- func input = case map (map readInt . words) (lines input) of
---     [[x]] -> unlines [showInt (24 - x)]
+func2 :: [String] -> [String]
+func2 inputs = case map words inputs of
+    _ -> map unwords []
